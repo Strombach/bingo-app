3676f949cffb57c76faa4fee4455255e3cd9de95
Fredrik Strömbäck <fredde.stromback@gmail.com>
2023-03-30 23:24:34 +0200

    builds a simple grid with colors

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  import arr from '../test.json';
+  import BingoTile from './components/BingoTile.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <h1>BINGO!!!</h1>
+    <div id="bingo_board">
+      <BingoTile v-for="obj in arr" :key="obj.id" :task="obj">{{ obj.activity }} - {{ obj.value }}</BingoTile>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  #bingo_board {
+    display: grid;
+    grid-template-columns: repeat(5, 20%);
+  }
 </style>
---
+++ src/components/BingoTile.vue
@@ -0,0 +1,32 @@
+<script setup>
+    const props = defineProps({
+        task: Object
+    })
+
+    function toggleState() {
+        console.log("HELLLO")
+        props.task.done = !props.task.done
+    }
+</script>
+
+<template>
+    <div class="bingo_tile" :class="{ 'task_done': task.done && !task.lifeline, 'lifeline': task.done && task.lifeline }"
+        @click="toggleState">
+        <h4>{{ task.activity }} - {{ task.id }}</h4>
+        <p>{{ task.value }}</p>
+    </div>
+</template>
+
+<style scoped>
+    .bingo_tile {
+        border-style: solid;
+    }
+
+    .task_done {
+        background-color: green;
+    }
+
+    .lifeline {
+        background-color: yellow;
+    }
+</style>
