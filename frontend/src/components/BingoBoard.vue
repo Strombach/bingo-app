49adb8c77443809256a8abaa17fe04bf0debba04
Fredrik Strömbäck <fredde.stromback@gmail.com>
2023-04-08 10:44:47 +0200

    now the svenne cannot see the taskform

--- frontend/src/components/BingoBoard.vue
+++ frontend/src/components/BingoBoard.vue
@@ -54,7 +54,7 @@
         <div id="bingo_board">
             <BingoTile v-for="obj in taskStore.allTasks" :key="obj.id" :task="obj" :onSelect="handleSelect"></BingoTile>
         </div>
-        <div id="tile_form">
+        <div v-if="userStore.user.userType !== 'svenne'" id="tile_form">
             <TileForm :onSave="updateTask"></TileForm>
         </div>
     </div>
